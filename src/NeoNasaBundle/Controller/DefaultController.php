f40f5dd084f9dd360ea85c0c101ea8f9202ab87e
setni <dupont.thomas70@gmail.com>
2016-08-29 15:15:10 +0200

    final
    Update with sensiolabs tester

--- src/NeoNasaBundle/Controller/DefaultController.php
+++ src/NeoNasaBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace NeoNasaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\Response;
 
 use NeoNasaBundle\Document\Neorepo;
 
@@ -27,34 +29,26 @@ class DefaultController extends Controller
         $tab = json_decode($data, true);
         foreach($tab['near_earth_objects'] as $date => $value) {
             foreach($value as $near_earth_object) {
-                
                 $neo_reference_id                   = $near_earth_object["neo_reference_id"];
                 $nom                                = $near_earth_object["name"];
                 $is_potentially_hazardous_asteroid  = $near_earth_object["is_potentially_hazardous_asteroid"];
                 $kilometres_per_hours               = $near_earth_object["close_approach_data"][0]["relative_velocity"]["kilometers_per_hour"];
-                
                 $repo = new Neorepo();
                 $repo->setDate($date);
                 $repo->setReference($neo_reference_id);
                 $repo->setNom($nom);
                 $repo->setHazardous($is_potentially_hazardous_asteroid);
                 $repo->setSpeed($kilometres_per_hours);
-                
                 $dm = $this->get('doctrine_mongodb')->getManager();
-                
                 try {
                     $dm->persist($repo);
                     $dm->flush();
                 } catch (Exception $e) {
                     return new Response('Error occured');
                 } 
-                
             }
-            
         }
-        return new Response('Insert OK');
-        
-        
+        return new Response('Insert OK'); 
     }
     public function hazardousAction () 
         {
@@ -99,7 +93,6 @@ class DefaultController extends Controller
                     'speed'     => $item->getSpeed()
                 );
             }
-            
             return new JsonResponse(array('result' => $arrayCollection));
         }
         
